cd4e0a452fa05b2aa9490b26e2693114ebbaa6a0
Musa <mbhc4235@gmail.com>
2023-10-19 16:00:37 +0300

    PlayerTestV1
    -Simple Square Player Added.
    -Movement Added.
    
    --------
    Files/Folders Added:
    -Materials Folder
    -PlayerMovement Script
    -Sprites Folder

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,15 +13,39 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+        Flip();
     }
+    
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
 }
