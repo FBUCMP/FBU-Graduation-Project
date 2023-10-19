4322048ace7ac0dfcae7d4f6f3b675fd4f896ab4
Musa <mbhc4235@gmail.com>
2023-10-19 16:03:23 +0300

    Update PlayerMovement.cs
    [SerializeField] Added for Movement Speed and JumpingPower

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
