31fc14d179424adb85bac02222ff268c321faaf0
Tiqsif <yekurtarici@gmail.com>
2023-10-29 12:49:01 +0300

    gizmo

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -37,6 +37,12 @@ public class PlayerMovement : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheckR.position, 0.2f, groundLayer) || Physics2D.OverlapCircle(groundCheckL.position, 0.2f, groundLayer);
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheckR.position, 0.2f);
+        Gizmos.DrawWireSphere(groundCheckL.position, 0.2f);
+    }
+
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
