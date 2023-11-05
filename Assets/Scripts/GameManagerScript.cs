d819694039f898f8a28f8d75ff4683fd14622337
Musa <mbhc4235@gmail.com>
2023-11-05 16:20:01 +0300

    GameManagerScript Update and Sıngleton System
    - Singleton System added

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -5,6 +5,43 @@ using UnityEngine;
 public class GameManagerScript : MonoBehaviour
 {
     public GameObject gameOverUI;
+
+
+    /// <summary>
+    /// Singleton deseni oluþturuyoruz yani oyun içerisinde sadece bir adet oluþturulmasý gereken ve oyun boyunca 
+    /// nesneleri kontrol edip, statik kalmasý gereken deðiþkenleri yönettiðimiz kod bloðu olacak.
+    /// blok buradan itibaren baþlamakta.
+    /// </summary>
+
+    private static GameManagerScript _instance; // Gamemanager´ýn bir adet kopyasýný/örneðini içeren kod satýrý.
+
+    public static GameManagerScript Instance //Instance adýnda bir özellik oluþturduk ve buradan get ile eriþim saðladýk.
+    {
+        get
+        {
+            if (_instance == null) // En baþta burasý var mý yok mu diye kontrol ediyoruz ve yoksa buradan bir kopya oluþturuyoruz.
+            {
+                _instance = FindObjectOfType<GameManagerScript>(); //Sahne içerisinde herhangi bir biçimde gamemanagerscript var mý diye kontrol ediyoruz. 
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = null;
+            DontDestroyOnLoad(this.gameObject); // Sahne deðiþtiðinde yok olmasýný engelliyoruz
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    /// BÝTÝÞ
     private void Start()
     {
         Time.timeScale = 1.0f;
