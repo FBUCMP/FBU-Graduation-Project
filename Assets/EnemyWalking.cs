d54221b5902123fd1ca364e738b36adf90b3d720
Erdogan Kilinc <erdogankilinc35@gmail.com>
2023-11-05 15:15:43 +0300

    Undo event and minor changes
    - Camera zoom script fixed and added in stage Scene
    - Undo merge operation with branch musa

---
+++ Assets/EnemyWalking.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWalking : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    bool isFacingRight;
+    private float wait = 0.1f;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent <Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFacingRight())
+        {
+            //Debug.Log("right");
+            // move right
+            rb.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            //Debug.Log("left");
+            // move left
+            rb.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    // enemy'nin yönünü deðiþtirmek için
+    private bool IsFacingRight()
+    {
+        return transform.localScale.x > Mathf.Epsilon;
+    }
+
+    // coroutine kullanarak enemy'nin yönünü deðiþtirmesini saðlýyoruz
+    private IEnumerator ChangeDirectionAfterDelay()
+    {
+        yield return new WaitForSeconds(wait);
+        transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // obje oluþmamýþsa coroutine çalýþmasýn diye yoksa hata verir
+        if (gameObject.activeSelf)
+        {
+            StartCoroutine(ChangeDirectionAfterDelay());
+        }
+    }
+}
