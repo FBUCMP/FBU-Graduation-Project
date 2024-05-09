a044e27678956bbbb6f2c3e75271a8220529b25e
Musa <mbhc4235@gmail.com>
2024-05-09 17:03:16 +0300

    Boss Animation Update and Upgrade UI Update

---
+++ Assets/Scripts/UI/CardButtonImage.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardButtonImage : MonoBehaviour
+{
+    public Image backgroundImage; // Kart arka planý için Image bileþeni
+
+    public Sprite whiteCardSprite; // Beyaz kart görseli
+    public Sprite greenCardSprite; // Yeþil kart görseli
+    public Sprite blueCardSprite; // Mavi kart görseli
+    public Sprite purpleCardSprite; // Mor kart görseli
+    public Sprite redCardSprite; // Kýrmýzý kart görseli
+    public void SetCardColor(string color)
+    {
+        // Renk ismine göre arka plan görselini ayarla
+        switch (color.ToLower())
+        {
+            case "white":
+                backgroundImage.sprite = whiteCardSprite;
+                break;
+            case "green":
+                backgroundImage.sprite = greenCardSprite;
+                break;
+            case "blue":
+                backgroundImage.sprite = blueCardSprite;
+                break;
+            case "purple":
+                backgroundImage.sprite = purpleCardSprite;
+                break;
+            case "red":
+                backgroundImage.sprite = redCardSprite;
+                break;
+            default:
+                break;
+        }
+    }
+}
