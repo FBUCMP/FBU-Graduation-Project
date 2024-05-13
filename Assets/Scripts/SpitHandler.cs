9aae0e35ffd8d36b72e415d33022a6c1b5a534e1
Murat Bektaş <93225280+muratbektas30@users.noreply.github.com>
2024-05-13 13:56:10 +0300

    New Boss Attack
    BabyFly boss attack added
    Boss animations fixed
    SalivaFire script name fixed

---
+++ Assets/Scripts/SpitHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpitHandler : MonoBehaviour
+{   
+    public FlyBossManager flyBossManager;
+    
+    void OnSpitEvent()
+    {
+        flyBossManager.SpawnSaliva();
+    }
+}
